1494918619133e1a1677c640282c86aa43534a9b
AndreyShulgin <neonod404@gmail.com>
2018-10-31 18:55:35 +0300

    1.3. JavaDoc. Code convention[#80648]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j.calculate;
+/**Первая программа 
+* выводит на экран Hello World!
+* @author Andrey Shulgin
+*/
 public class Calculate{
+	/** Точка входа в класс
+	* @param args Массив строковых аргументов
+	*/
 	public static void main (String[] args){
 		System.out.println("Helo World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Shulgin (neonod404@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
