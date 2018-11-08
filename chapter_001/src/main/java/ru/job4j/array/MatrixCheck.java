51b8d5c85022067e849ac28931a657f76de5d442
AndreyShulgin <neonod404@gmail.com>
2018-11-09 00:50:09 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.[#80629]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -12,11 +12,10 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
 
-        for (int i = 1; i < data.length; i++) {
-            for (int j = 1; j < data.length; j++) {
-                if (i == j && data[i][j] != data[i -1][j -1] || i == data.length - 1 - j && data[i][data.length - 1 - j] != data[i-1][data.length -2 -j]) {
-                    result = false;
-                }
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i][i] != data [i + 1][i + 1] || data[i][data.length - 1 - i] != data[i + 1][data.length  - 2 - i]) {
+                result = false;
+                break;
             }
         }
         return result;
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -6,28 +6,52 @@ import static org.junit.Assert.assertThat;
 
 public class MatrixCheckTest {
     @Test
-    public void whenDiagonalFalseOrTrueThenTrue(){
-        MatrixCheck matrix = new MatrixCheck();
-        boolean [][] table = {
+    public void whenDataMonoByTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][]{
                 {true, true, true},
                 {false, true, true},
                 {true, false, true}
         };
-        boolean result  = matrix.mono(table);
+        boolean result = check.mono(input);
         assertThat(result, is(true));
     }
 
     @Test
-    public void whenDiagonalFalseOrTrueThenTrue2(){
+    public void whenDataNotMonoByTrueThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][]{
+                {true, true, false},
+                {false, false, true},
+                {true, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDiagonalFalseOrTrueThenTrue2() {
         MatrixCheck matrix = new MatrixCheck();
-        boolean [][] table = {
+        boolean[][] table = {
                 {true, false, false, true},
                 {false, true, true, false},
                 {false, true, true, false},
                 {true, false, false, true}
         };
-        boolean result  = matrix.mono(table);
+        boolean result = matrix.mono(table);
         assertThat(result, is(true));
+    }
 
+    @Test
+    public void whenDiagonalFalseOrTrueThenTrue() {
+        MatrixCheck matrix = new MatrixCheck();
+        boolean[][] table = {
+                {true, false, false, true},
+                {false, true, true, false},
+                {true, true, true, false},
+                {false, false, false, true}
+        };
+        boolean result = matrix.mono(table);
+        assertThat(result, is(false));
     }
 }
