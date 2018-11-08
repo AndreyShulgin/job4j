1995c8e5b7cbf45caf412ee5a21b4d073662823b
AndreyShulgin <neonod404@gmail.com>
2018-11-08 22:26:59 +0300

    6.3. Массив заполнен true или false[#80628]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -11,11 +11,13 @@ public class Check {
      * @return возвращает true если массив состоит только из True или False. И False если нет.
      */
     public boolean mono(boolean[] data) {
+        boolean result = true;
       for (int index = 1; index < data.length; index++) {
           if (data[index] != data[index - 1]) {
-              return false;
+              result = false;
+              break;
           }
        }
-        return true;
+        return result;
     }
 }
