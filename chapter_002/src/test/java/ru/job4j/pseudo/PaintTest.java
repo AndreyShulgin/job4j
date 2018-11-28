662c389054d645cf022691ccee505b380a9d14a6
AndreyShulgin <neonod404@gmail.com>
2018-11-28 16:09:54 +0300

    4. Используя шаблон проектирования - стратегию[#80688]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -25,10 +25,23 @@ public class PaintTest {
         // проверяем результат вычисления
         assertThat(
                 new String(out.toByteArray()),
-                is(
-                        new StringBuilder().append("++++\n").append("++++\n").append("++++\n").append("++++").append(System.lineSeparator()).toString()
-                ));
+                is(new StringBuilder().append("++++\n").append("++++\n").append("++++\n").append("++++").append(System.lineSeparator()).toString()));
         // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
+    @Test
+    public void whenDrawTriangle() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Triangle());
+        assertThat(new String(out.toByteArray()), is(
+                new StringBuilder()
+                            .append("   0\n")
+                            .append("  000\n")
+                            .append(" 00000\n")
+                            .append("0000000")
+                            .append(System.lineSeparator()).toString()));
         System.setOut(stdout);
     }
 }
