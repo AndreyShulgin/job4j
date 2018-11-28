adc9ad4e70339b874642d25fa2a3e16e2c3fc81a
AndreyShulgin <neonod404@gmail.com>
2018-11-28 16:20:56 +0300

    5. Рефакторинг теста @Before @After[#80683]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -12,28 +14,30 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().draw(new Square());
-        // проверяем результат вычисления
         assertThat(
                 new String(out.toByteArray()),
-                is(new StringBuilder().append("++++\n").append("++++\n").append("++++\n").append("++++").append(System.lineSeparator()).toString()));
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
+                is(new StringBuilder()
+                        .append("++++\n")
+                        .append("++++\n")
+                        .append("++++\n")
+                        .append("++++")
+                        .append(System.lineSeparator()).toString()));
     }
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(new String(out.toByteArray()), is(
                 new StringBuilder()
@@ -42,6 +46,5 @@ public class PaintTest {
                             .append(" 00000\n")
                             .append("0000000")
                             .append(System.lineSeparator()).toString()));
-        System.setOut(stdout);
     }
 }
