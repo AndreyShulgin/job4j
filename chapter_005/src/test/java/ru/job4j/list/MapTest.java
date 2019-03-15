8d4d3dc372f91208b5d4acc024f69d9e2d6a0797
AndreyShulgin <neonod404@gmail.com>
2019-03-16 02:31:33 +0300

    2. Не перекрывать equals hashCode[#107302]

--- chapter_005/src/test/java/ru/job4j/list/MapTest.java
+++ chapter_005/src/test/java/ru/job4j/list/MapTest.java
@@ -1,6 +1,10 @@
 package ru.job4j.list;
 
+import org.junit.Test;
+
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapTest {
 
@@ -14,5 +18,57 @@ public class MapTest {
             this.children = children;
             this.birthday = birthday;
         }
+    }
+
+    @Test
+    public void map() {
+        Calendar calendar = new Calendar() {
+            @Override
+            protected void computeTime() {
+
+            }
+
+            @Override
+            protected void computeFields() {
+
+            }
+
+            @Override
+            public void add(int field, int amount) {
+
+            }
+
+            @Override
+            public void roll(int field, boolean up) {
+
+            }
+
+            @Override
+            public int getMinimum(int field) {
+                return 0;
+            }
+
+            @Override
+            public int getMaximum(int field) {
+                return 0;
+            }
+
+            @Override
+            public int getGreatestMinimum(int field) {
+                return 0;
+            }
+
+            @Override
+            public int getLeastMaximum(int field) {
+                return 0;
+            }
+        };
+        User user1 = new User("Andrey", 0, calendar);
+        User user2 = new User("Andrey", 0, calendar);
+        Map<User, Object> map = new HashMap<>();
+
+        map.put(user1, 1);
+        map.put(user2, 2);
+        System.out.println(map);
     }
 }
