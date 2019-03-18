d3aa4e8edfb06cd23ca65ca8c917f497ccfb0d7d
AndreyShulgin <neonod404@gmail.com>
2019-03-18 16:05:49 +0300

    4. Переопределить только equals[#107301]

---
+++ chapter_005/src/main/java/ru/job4j/list/User.java
@@ -0,0 +1,92 @@
+package ru.job4j.list;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday);
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = new Calendar() {
+            @Override
+            protected void computeTime() {
+
+            }
+
+            @Override
+            protected void computeFields() {
+
+            }
+
+            @Override
+            public void add(int field, int amount) {
+
+            }
+
+            @Override
+            public void roll(int field, boolean up) {
+
+            }
+
+            @Override
+            public int getMinimum(int field) {
+                return 0;
+            }
+
+            @Override
+            public int getMaximum(int field) {
+                return 0;
+            }
+
+            @Override
+            public int getGreatestMinimum(int field) {
+                return 0;
+            }
+
+            @Override
+            public int getLeastMaximum(int field) {
+                return 0;
+            }
+        };
+        User user1 = new User("Andrey", 0, calendar);
+        User user2 = new User("Andrey", 0, calendar);
+        Map<User, Object> map = new HashMap<>();
+
+        map.put(user1, 1);
+        map.put(user2, 2);
+        System.out.println(map);
+        System.out.println(user1.equals(user2));
+    }
+}
